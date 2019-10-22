dfd6157312278bd5b1ed2f8578173ed5c9333683
Baldemar Sepulveda <balde.sepu@pm.me>
2019-10-21 21:21:12 -0700

    Updates

---
+++ precache-manifest.de7e55220f683625aebfe9af06dc766a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8699c12b48c6669fd56bff961d40ee66",
+    "url": "/react-pagination-app/index.html"
+  },
+  {
+    "revision": "143b26c6c674aac9a90d",
+    "url": "/react-pagination-app/static/js/2.d565602c.chunk.js"
+  },
+  {
+    "revision": "762aede204a28b1346b1",
+    "url": "/react-pagination-app/static/js/main.4a6dc7fe.chunk.js"
+  },
+  {
+    "revision": "0ec12e4bd88f6761dd16",
+    "url": "/react-pagination-app/static/js/runtime-main.ef8cffc7.js"
+  }
+]);
